40739b535380e5395f2819fce5f4c9e9045640b1
Kurt Lachmann <lachmannk@gmail.com>
2022-04-28 23:07:54 +0200

    Progress on the UI prototype

--- src/rechteck_qs/RechteckQS.tsx
+++ src/rechteck_qs/RechteckQS.tsx
@@ -1,5 +1,5 @@
 import { jsx } from '@emotion/react'
-import { Container, Divider, Grid, Input, InputAdornment, styled, TextField, Typography } from '@mui/material';
+import { AppBar, Container, Divider, Grid, Input, InputAdornment, List, ListItem, Paper, Stack, styled, TextField, Toolbar, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import React from 'react';
 import BetonSelector from './BetonSelector';
@@ -12,114 +12,272 @@ const Label = styled(Typography)(({ theme }) => ({
 }));
 
 export default function RechteckQS() {
-	return (
-		<Container maxWidth="sm">
-			<Typography variant="h4" align="center">Rechteck Querschnitt</Typography>
-			<Grid container spacing={2} justifyContent="center" alignItems="center">
-				<Grid item xs={2}>
-					<Label>Beton</Label>
-				</Grid>
-				<Grid item xs={10}>
-					<BetonSelector />
-				</Grid>
+	return <>
+		<AppBar>
+			<Toolbar>
+				<Typography variant="h6">
+					Rechteck Querschnitt
+				</Typography>
+			</Toolbar>
+		</AppBar>
+		<Container maxWidth="sm" style={{ marginTop: "70px" }}>
+			<List>
+				<ListItem>
+					<Paper elevation={3} style={{ backgroundColor: "#fafafa", width: "100%" }}>
+						<List>
+							<ListItem>
+								<Stack direction="row">
+									<Label>Beton</Label>
+									<BetonSelector />
+								</Stack>
+							</ListItem>
+						</List>
+					</Paper>
+				</ListItem>
 
-				<Grid item xs={2}>
-					<Label>Stahl</Label>
-				</Grid>
-				<Grid item xs={10}>
-					<StahlSelector />
-				</Grid>
+				<ListItem>
+					<Paper elevation={3} style={{ backgroundColor: "#fafafa", width: "100%" }}>
+						<List>
+							<ListItem>
+								<Stack direction="row">
+									<Label>Baustahl</Label>
+									<Label>B500</Label>
+								</Stack>
+							</ListItem>
+							<Divider />
+							<ListItem>
+								<Label style={{ minWidth: "3em" }}>A<sub>s1</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+											<Typography style={{ marginTop: "-0.5rem", fontSize: "0.8rem" }}>
+												2
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<Label style={{ minWidth: "3em" }}>A<sub>s2</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+											<Typography style={{ marginTop: "-0.5rem", fontSize: "0.8rem" }}>
+												2
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<Label style={{ minWidth: "3em" }}>d<sub>1</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+						</List>
+					</Paper>
+				</ListItem>
 
-				<Grid item xs={4}>
-					<Label>A<sub>s1</sub></Label>
-				</Grid>
-				<Grid item xs={8}>
-					<TextField
-						defaultValue={"20.0"}
-						fullWidth={true}
-						InputProps={{
-							inputProps: {
-								style: {textAlign: "right"}
-							},
-							endAdornment: <InputAdornment position="end">
-								<Typography>
-									cm
-								</Typography>
-								<Typography style={{marginTop: "-0.5rem", fontSize: "0.8rem"}}>
-									2
-								</Typography>
-							</InputAdornment>
-						  }}
-					/>
-				</Grid>
+				<ListItem>
+					<Paper elevation={3} style={{ backgroundColor: "#fafafa", width: "100%" }}>
+						<List>
+							<ListItem>
+								<Stack direction="row">
+									<Label>Spannstahl</Label>
+									<StahlSelector />
+								</Stack>
+							</ListItem>
+							<Divider />
+							<ListItem>
+								<Label style={{ minWidth: "3em" }}>E<sub>p</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												N/mm
+											</Typography>
+											<Typography style={{ marginTop: "-0.5rem", fontSize: "0.8rem" }}>
+												2
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<Label style={{ minWidth: "3em" }}>A<sub>p</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+											<Typography style={{ marginTop: "-0.5rem", fontSize: "0.8rem" }}>
+												2
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<Label style={{ minWidth: "3em" }}>d<sub>p</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+						</List>
+					</Paper>
+				</ListItem>
 
-				<Grid item xs={4}>
-					<Label>A<sub>s2</sub></Label>
-				</Grid>
-				<Grid item xs={8}>
-					<TextField
-						defaultValue={"0.0"}
-						fullWidth={true}
-						InputProps={{
-							inputProps: {
-								style: {textAlign: "right"}
-							},
-							endAdornment: <InputAdornment position="end">
-								<Typography>
-									cm
-								</Typography>
-								<Typography style={{marginTop: "-0.5rem", fontSize: "0.8rem"}}>
-									2
-								</Typography>
-							</InputAdornment>
-						  }}
-					/>
-				</Grid>
+				<ListItem>
+					<Paper elevation={3} style={{ backgroundColor: "#fafafa", width: "100%" }}>
+						<List>
+							<ListItem>
+								<Label>Querschnitt</Label>
+							</ListItem>
+							<Divider />
+							<ListItem>
+								<Label style={{ minWidth: "5em" }}>Breite b</Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<Label style={{ minWidth: "5em" }}>Höhe h</Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												cm
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+						</List>
+					</Paper>
+				</ListItem>
 
-				<Grid item xs={4}>
-					<Label>d<sub>1</sub></Label>
-				</Grid>
-				<Grid item xs={8}>
-					<TextField
-						defaultValue={"20.0"}
-						fullWidth={true}
-						InputProps={{
-							inputProps: {
-								style: {textAlign: "right"}
-							},
-							endAdornment: <InputAdornment position="end">
-								<Typography>
-									cm
-								</Typography>
-							</InputAdornment>
-						  }}
-					/>
-				</Grid>
+				<ListItem>
+					<Paper elevation={3} style={{ backgroundColor: "#fafafa", width: "100%" }}>
+						<List>
+							<ListItem>
+								<Label>Einwirkung</Label>
+							</ListItem>
+							<Divider />
+							<ListItem>
+								<Label style={{ minWidth: "5em" }}>N<sub>Ed</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												kN
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+							<ListItem>
+								<Label style={{ minWidth: "5em" }}>M<sub>Ed</sub></Label>
+								<TextField
+									defaultValue={"20.0"}
+									fullWidth={true}
+									size="small"
+									InputProps={{
+										inputProps: {
+											style: { textAlign: "right" }
+										},
+										endAdornment: <InputAdornment position="end">
+											<Typography>
+												kN
+											</Typography>
+										</InputAdornment>
+									}}
+								/>
+							</ListItem>
+						</List>
+					</Paper>
+				</ListItem>
+			</List>
 
-				<Grid item xs={4}>
-					<Label>d<sub>2</sub></Label>
-				</Grid>
-				<Grid item xs={8}>
-					<TextField
-						defaultValue={"5.0"}
-						fullWidth={true}
-						InputProps={{
-							inputProps: {
-								inputMode: "numeric",
-								pattern: "[0-9]*",
-								style: {textAlign: "right"}
-							},
-							endAdornment: <InputAdornment position="end">
-								<Typography>
-									cm
-								</Typography>
-							</InputAdornment>
-						  }}
-					/>
-				</Grid>
-			</Grid>
-			{/* <Button variant="contained">Hello World</Button>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. */}
 		</Container>
-	)
+	</>
 }
