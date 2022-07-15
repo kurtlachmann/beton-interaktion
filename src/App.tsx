fe7f295e3f14cecdbc76dbaba7a2f51f5c244649
Kurt Lachmann <lachmannk@gmail.com>
2022-07-15 17:50:05 +0200

    Remove stats workflow
    The workflow was kind of hacky and unreliable.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import RechteckQS from './rechteck_qs/RechteckQS';
-import Stats from './stats/Stats';
 
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route path='/stats' element={<Stats />} />
         <Route path='/' element={<RechteckQS />} />
       </Routes>
     </React.Fragment>
